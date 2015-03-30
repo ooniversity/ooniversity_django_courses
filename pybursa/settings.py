8fd9cc73b1758b64aeace1751b2b6c5552fd4d97
Paha <paha@ubuntu.ubuntu-domain>
2015-03-31 01:53:43 +0300

    Create polls

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'polls',
 )
 
 ROOT_URLCONF = 'pybursa.urls'
