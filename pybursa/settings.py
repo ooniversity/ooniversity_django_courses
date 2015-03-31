80305ead10b2f1f4bbc9f59b56f715591d9cc130
Oksana Slusarenko <oksana.slu@gmail.com>
2015-03-31 18:06:37 +0300

    added models student

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'polls',
     'quadratic',
     'courses',
+    'students',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ students/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from courses.models import Course
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    skype = models.CharField(max_length=255)
+    course = models.ManyToManyField(Course)
+
+    def __unicode__(self):
+        return '{} {}'.format(self.name, self.surname)
