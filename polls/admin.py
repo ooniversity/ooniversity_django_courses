e23bc5719c4a7e130cd2f11b12c2f75626e85cca
Paha <paha@ubuntu.ubuntu-domain>
2015-03-31 23:44:53 +0300

    Add folder Admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Choice, Question
 
-dmin.site.register(Question)
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -14,3 +14,9 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __unicode__(self):
     	return self.choice_text
+    	
+def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+was_published_recently.admin_order_field = 'pub_date'
+was_published_recently.boolean = True
+was_published_recently.short_description = 'Published recently?'
