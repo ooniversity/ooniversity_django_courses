2baf3384cb0c948c1d40d1b55407473d71961bea
Oksana Slusarenko <oksana.slu@gmail.com>
2015-04-01 17:10:18 +0300

    configured CourseAdmin and StudentAdmin

--- students/admin.py
+++ students/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from students.models import Student
 
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'skype')
+    list_filter = ['courses']
+    search_fields = ['surname', 'email']
+    filter_horizontal = ('courses',)
+
+    fieldsets = (
+        ('Personal info', {'fields': ('name', 'surname', 'date_of_birth')}),
+        ('Contact info', {'fields': ('email', 'phone', 'address', 'skype')}),
+        (None, {'fields': ('courses',)}),
+    )
+
+    def full_name(self, obj):
+        return u"{} {}".format(obj.name, obj.surname)
+    full_name.short_description = 'full name'
+
+
+admin.site.register(Student, StudentAdmin)
