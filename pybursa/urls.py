aa8a15c5587f9d405ddfeb3ef6128d629d207511
Alexey <you@example.com>
2015-04-01 09:41:24 +0300

    Task 3 - urls rebuilt

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from pybursa.views import index, contact, student_list, student_detail
+
 
 urlpatterns = patterns('',
+    url(r'^$', index, name='home'),
+    url(r'^contact/$', contact),
+    url(r'^student_list/$', student_list),
+    url(r'^student_detail/$', student_detail),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
