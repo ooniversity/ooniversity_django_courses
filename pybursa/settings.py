d65a0b1cae6919c7ea3da8d745185ae0b980b65b
Olga Kotulska <o.kotulska@gmail.com>
2015-04-01 13:51:43 +0300

    renedering new pages

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -78,6 +78,8 @@ USE_L10N = True
 USE_TZ = True
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
--- pybursa/urls.py
+++ pybursa/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pybursa.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'pybursa.views.index', name='index'),
+    url(r'^contact/$', 'pybursa.views.contact', name='contact'),
+    url(r'^student_list/$', 'pybursa.views.student_list', name='student_list'),
+    url(r'^student_detail/$', 'pybursa.views.student_detail', name='student_detail'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
