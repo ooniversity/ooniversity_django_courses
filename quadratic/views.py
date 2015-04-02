6c743066b4677266c62df3540d5a01b2a7d39eab
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-03 01:07:03 +0300

    Create function for find quadratic

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,3 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 from django.shortcuts import render
 
-# Create your views here.
+def validate(request, name):
+    value = request.GET.get(name)
+    if value == '' or value == None:
+        return value, "Коэфициент не определен"
+
+    try:
+        return int(value), None
+    except ValueError:
+        return value, "Коэфициент не целое число"
+
+
+def quadratic_results(request):
+    a, a_error = validate(request, 'a')
+    b, b_error = validate(request, 'b')
+    c, c_error = validate(request, 'c')
+
+    if a_error is None and a == 0:
+            a_error = ("Коэфициент при первом слагаемом уравнении не может быть"
+                " равен нулю")
+
+    discr = None
+    x1 = None
+    x2 = None
+
+    if not any([a_error, b_error, c_error]):
+        discr = b ** 2 - 4 * a * c
+
+        if discr == 0:
+            x1 = x2 = -b / 2*a
+        elif discr > 0:
+            x1 = (-b + discr ** 0.5) / (2 * a)
+            x2 = (-b - discr ** 0.5) / (2 * a)
+
+    return render(request, 'quadratic_results.html', {
+        'a':a,
+        'b':b,
+        'c':c,
+        'discr':discr,
+        'a_error': a_error,
+        'b_error': b_error,
+        'c_error': c_error,
+        'x1': x1,
+        'x2': x2,
+    })
