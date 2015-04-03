7aed5529e31fcd1a07ba55668b64099d7b29deb9
Maxim Bilozorov <maximbilozorov@yandex.ua>
2015-04-03 21:57:21 +0300

    django 3 finished

---
+++ pybursa/pybursa/urls.py
@@ -0,0 +1,57 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
+
+#def test(request):
+#	print "\n--------------------------------------"
+#	print request.path
+#	print request.method
+#	print request.GET, request.POST
+#	response = render(request,'test.html')
+#	print response.status_code
+#	print response['Content-Type']
+#	response['Age_MAX!!!'] = 20000
+#	return HttpResponseNotFound('DDD!')
+#
+#def hello(request):
+#	return render(request,'index.html')
+#	#return HttpResponse('Hello World!')
+#
+#def hello_python(request):
+#	return HttpResponse("Hello Python!")
+#
+#def sum_two(request, a, b):
+#	print a, b
+#	d = int(a)+int(b)
+#	return HttpResponse('Your result is: % d' % d)
+
+def index(request):
+	return render(request,'index.html')
+
+
+def contact(request):
+	return render(request,'contact.html')
+
+
+def student_detail(request):
+	return render(request,'student_detail.html')
+
+
+def student_list(request):
+	return render(request,'student_list.html')
+
+urlpatterns = patterns('',
+    url(r'^contact/$', contact, name="contact"),
+    url(r'^student_list/$', student_list, name="student_list"),
+    url(r'^student_detail/$', student_detail, name="student_detail"),
+    url(r'^$', index, name="home"),
+    # Examples:
+    #url(r'^$', index, name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    #url(r'^python/$', hello_python),
+    #url(r'^test/$', test),
+    #url(r'^python/(?P<version>2|3)/$', hello_python),
+    #url(r'^sum/(?P<a>\d+)/(?P<b>\d+)/$', sum_two),
+    #url(r'^admin/', include(admin.site.urls)),
+)
