5764a0b11864b5e52f050f758ef3104c3c7822f4
Alexey <you@example.com>
2015-04-03 09:53:31 +0300

    Quadratic equation - first step

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from quadratic.views import results
+from views import results
 
 
 urlpatterns = patterns('',
--- quadratic/views.py
+++ quadratic/views.py
@@ -1,9 +1,22 @@
+#coding: utf-8
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from quadratic import check_coef, solve_quadratic_equation
 
 
 def results(request):
     a = request.GET['a']
     b = request.GET['b']
     c = request.GET['c']
-    return HttpResponse ("a = {0}, b = {1}, c = {2}".format(a, b, c))
+    text = 'Квадратное уравнение a*x*x + b*x + c = 0'
+    text += '\n• a = {0}\n'.format(a)
+    text += check_coef(a, isA = True)
+    text += '\n• b = {0}\n'.format(b)
+    text += check_coef(b)
+    text += '\n• c = {0}\n'.format(c)
+    text += check_coef(c)
+    if check_coef(a, isA = True) == '' and check_coef(b) == '' and check_coef(c) == '':
+        text += 'Дискриминант: {0}\n'.format()   #Дописать вычисление дискриминанта!
+        text += solve_quadratic_equation(a, b, c)
+    return HttpResponse (text)
