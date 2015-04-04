3a558b5aa1500b4b1691fcabf3c1dfeb050da608
Volodya Esk <vkhatianovskyi@gmail.com>
2015-04-04 21:13:41 +0300

    Almost done, MVC pattern on, files work on Dev Server

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]###############
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -83,3 +81,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import pybursa  
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pybursa.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'pybursa.views.index', name='home'),
+	url(r'^index/$', 'pybursa.views.index', name='home'),
+    url(r'^contact/$', 'pybursa.views.contact'),
+	url(r'^student_list/$', 'pybursa.views.student_list'),
+	url(r'^student_detail/$', 'pybursa.views.student_detail'),
+
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
