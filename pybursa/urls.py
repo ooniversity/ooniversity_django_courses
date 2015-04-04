78e88069265be5af58011d283143f7580f9904c6
Kostiantyn Moskalenko <con.mosk@gmail.com>
2015-04-05 00:27:27 +0300

    kz3_2

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -8,4 +8,8 @@ urlpatterns = patterns('',
 
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'pybursa.views.dj101_index'),
+    url(r'^contact/$', 'pybursa.views.dj101_contact'),
+    url(r'^student_list/$', 'pybursa.views.dj101_student_list'),
+    url(r'^student_detail/$', 'pybursa.views.dj101_student_detail'),
 )
