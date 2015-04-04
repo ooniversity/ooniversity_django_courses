c807bc3c123c7c69f621244d94be8bdfe5667dc7
Volodya Esk <vkhatianovskyi@gmail.com>
2015-04-04 23:22:43 +0300

    Urls are modified with {%url%} tag

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -4,11 +4,11 @@ import pybursa
 
 
 urlpatterns = patterns('',
-    url(r'^$', 'pybursa.views.index', name='home'),
-	url(r'^index/$', 'pybursa.views.index', name='home'),
-    url(r'^contact/$', 'pybursa.views.contact'),
-	url(r'^student_list/$', 'pybursa.views.student_list'),
-	url(r'^student_detail/$', 'pybursa.views.student_detail'),
+    url(r'^$', 'pybursa.views.index', name='index'),
+	url(r'^index/$', 'pybursa.views.index', name='index'),
+    url(r'^contact/$', 'pybursa.views.contact', name='contact'),
+	url(r'^student_list/$', 'pybursa.views.student_list', name='student_list'),
+	url(r'^student_detail/$', 'pybursa.views.student_detail', name='student_detail'),
 
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
