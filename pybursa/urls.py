2f72222647b4d411ce4e55ac3813780d153ad76c
TeplovAlexander <sasha_tep@mail.ru>
2015-04-04 22:47:01 +0300

    Edited templates-html - Django / Control 3 / Zadanie 2 !

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from pybursa.views import index, contact, student_detail, student_list
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,5 +12,10 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^polls/', include('polls.urls', namespace = "polls")),
+
+    url(r'^$', index, name = 'index'),
+    url(r'^contact/$', contact, name = 'contact'),
+    url(r'^student_detail/$', student_detail, name = 'student_detail'),
+    url(r'^student_list/$', student_list, name = 'student_list'),
 
 )
