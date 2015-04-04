ae1fc84044e07e330a97bef4e4226cac32a707b9
Олег Харюк <khfzar65@mail.ru>
2015-04-05 00:55:09 +0300

    Доработка переходов

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -4,10 +4,10 @@ from pybursa.views import pybursa, pybursa_contact, pybursa_student_list, pyburs
  
 
 urlpatterns = patterns('',
-    url(r'^$', pybursa),
-    url(r'^contact/$', pybursa_contact),
-    url(r'^student_list/$', pybursa_student_list),
-    url(r'^student_detail/$', pybursa_student_detail),
+    url(r'^$', pybursa, name='home'),
+    url(r'^contact/$', pybursa_contact, name='contact'),
+    url(r'^student_list/$', pybursa_student_list, name='student_list'),
+    url(r'^student_detail/$', pybursa_student_detail, name='student_detail'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
