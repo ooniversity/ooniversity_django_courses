e7cb7fac97ce8bed0bc9240efc804bbee1ae4c11
andreyo <ao.mail@ua.fm>
2015-04-04 23:08:12 +0300

    task 3_2. url routing & static added for 4 html files - finished

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
+from pybursa import views
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pybursa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', views.index, name='index'),
+    #url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^student_list/', views.student_list, name='student_list'),
+    url(r'^student_detail/', views.student_detail, name='student_detail'),
 
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
