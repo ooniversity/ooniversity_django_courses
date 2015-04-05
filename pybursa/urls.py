d900a4d528e23ac2d014a748d151e9c93d3ab5dd
Pavel Kozlov <tvink.odin@gmail.com>
2015-04-05 16:07:21 +0400

    Django Week 2 KZ 3

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from pybursa.views import index, contact, student_list, student_detail
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pybursa.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', index),
+    url(r'^contact/$', contact),
+    url(r'^student_list/$', student_list),
+    url(r'^student_detail/$', student_detail),
 
-    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
