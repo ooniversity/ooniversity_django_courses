e0e4d7f7e84e3042d24f6543f0b3d6ba9b1f28b3
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2015-04-05 10:07:54 +0300

    Task Quadratic. Optimisated code

--- quadratic/views.py
+++ quadratic/views.py
@@ -13,6 +13,4 @@ def quadratic_results (request):
 
     #Парсим HTML страничку и передаем ей все необходимие переменные
     return render (request, 'quadratic/index_quadratic.html',
-        {'a':dict_quadr['a'], 'b':dict_quadr['b'], 'c':dict_quadr['c'],
-        'a_native':request.GET['a'], 'b_native':request.GET['b'], 'c_native':request.GET['c'],
-        'd':dict_quadr['d'], 'x_1':dict_quadr['x_1'], 'x_2':dict_quadr['x_2'], 'result':dict_quadr['result']})
+        {'a_native':request.GET['a'], 'b_native':request.GET['b'], 'c_native':request.GET['c'],  'diction':dict_quadr},)
