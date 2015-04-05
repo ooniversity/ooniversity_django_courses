e361e20eb96e12b7cdc21a1efdacc804a7cada87
Kirill Zvyagintsev <zvyagintsev.education@gmail.com>
2015-04-05 19:29:28 +0300

    complete Task 3

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -83,4 +83,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATES_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
