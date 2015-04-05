acd86d61f0702aa4d3941cbb1d1c750215c7251a
Karakash Sergey <karakash.sergey@gmail.com>
2015-04-05 19:14:22 +0300

    fixed html view issues - 2

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
     url(r'^student_list/$', student_list, name='student_list'),
     url(r'^student_detail/$', student_detail, name='student_detail'),
     
+
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
