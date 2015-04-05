783cf85a88bc3336e12f604087e71f73cddb88b0
Kirill Zvyagintsev <zvyagintsev.education@gmail.com>
2015-04-06 00:31:38 +0300

    Completed task

--- quadratic/resolver.py
+++ quadratic/resolver.py
@@ -1,9 +1,10 @@
 def check_value(value):
     result=''
     if value:
-        if isinstance(value, int):
-            result = [True ,'']
-        else:
+        try:
+        	int(value)
+        	result = [True ,'']
+        except ValueError:
             result = [False, '\nCoefficient is not integer']
     else:
         result = [False , "\nCoefficient is empty"]
@@ -15,11 +16,11 @@ def discr(a, b, c):
 def check_discr(d):
     result = ''
     if d < 0:
-        result = [False, '<br>Discriminant less then 0, equation has no solutions']
+        result = [False, 'Discriminant less then 0, equation has no solutions ']
     elif d > 0:
-        result = [True, '<br>Equation has two solutions']
+        result = [True, 'Equation has two solutions ']
     else:
-        result = [True, '<br>Equation has one solutions']
+        result = [True, 'Equation has one solutions ']
     return result
 
 def find_solution(a, b, d):
@@ -29,18 +30,23 @@ def find_solution(a, b, d):
     return s
 
 def resolve(a=None, b=None, c=None):
-    result = ''
+    solution = {'result':'', 'discr':''}
     if not(check_value(a)[0] and check_value(b)[0] and check_value(c)[0]):
-        return result
-
+        return solution
+    a = int(a)
+    b = int(b)
+    c = int(c)
     d = discr(a, b, c)
+    solution['discr'] = 'Discriminant= '+ str(d)
     if check_discr(d)[0]:
         if d == 0:
-            result += "Discriminant = 0"
-            return result + check_discr(d)[1] + "x1=x2" + str(-b/2*a)
+            solution['result'] = solution['result'] + check_discr(d)[1] + "x1=x2= " + str(-b/2*a)
         else:
-            result += "Discriminant =" + str(d) 
             s = find_solution(a, b, d)
-            return result + check_discr(d)[1] + "x1 = " + str(s[0]) + "x2 = " + str(s[1])
+            solution['result'] = solution['result']+ check_discr(d)[1] + "x1 = " + str(s[0]) + " x2 = " + str(s[1])
     else:
-        return result + check_discr(d)[1]
+        solution['result'] = solution['result'] + check_discr(d)[1]
+    return solution
+
+
+print resolve(1, 10, 3)
--- quadratic/views.py
+++ quadratic/views.py
@@ -5,8 +5,10 @@ def results(request):
 	a = request.GET['a']
 	b = request.GET['b']
 	c = request.GET['c']
-	a_result = int(resolver.check_value(a)[0])
-    #b_result = resolver.check_value(b)
-    #c_result = resolver.check_value(c)
-    #result = resolver.resolve(a, b, c)
-	return render(request, "results.html",{'a': a,'a_result': a_result})
+	a_result = resolver.check_value(a)[1]
+	b_result = resolver.check_value(b)[1]
+	c_result = resolver.check_value(c)[1]
+	answer = resolver.resolve(a, b, c)
+	result = answer['result']
+	discr = answer['discr']
+	return render(request, "results.html",{'a': a, 'b': b, 'c': c,'a_result': a_result, 'b_result': b_result, 'c_result': c_result, 'result': result, 'd':discr})
