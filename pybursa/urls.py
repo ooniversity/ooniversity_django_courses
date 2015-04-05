0d7e79621f98ad165c984eff521cb7bef0fa65e3
Karakash Sergey <karakash.sergey@gmail.com>
2015-04-05 18:48:38 +0300

    fixed html view issues

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from pybursa.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from pybursa.views import index, contact, student_list, student_detail
+
+from pybursa import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pybursa.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', home, name='home'),
+    url(r'^$', index, name='index'),
     url(r'^contact/$', contact, name='contact'),
     url(r'^student_list/$', student_list, name='student_list'),
     url(r'^student_detail/$', student_detail, name='student_detail'),
+    
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
