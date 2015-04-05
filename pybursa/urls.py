72678680e0dd97786ef4732d1d9ca42edd57b2c4
Олег Харюк <khfzar65@mail.ru>
2015-04-05 23:01:58 +0300

    kz4

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     url(r'^student_list/$', pybursa_student_list, name='student_list'),
     url(r'^student_detail/$', pybursa_student_detail, name='student_detail'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^quadratic/', include('quadratic.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
