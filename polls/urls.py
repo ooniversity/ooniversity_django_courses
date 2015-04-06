5642fdbd9786768563e3a2ee7c6b43535bc97872
Maxim Bilozorov <maximbilozorov@yandex.ua>
2015-04-06 23:55:45 +0300

    add POLLS task

---
+++ polls/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, url
+
+from polls import views
+
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+
+
+   url(r'^specifics/(?P<question_id>\d+)/$', views.detail, name='detail'),
+   url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+)
