76b02178417911c45cae0d58a8fb3872e5ff63dd
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-07 23:36:07 +0300

    Add two function
    * index_courses - to show all courses
    * course_show - to show detail information about each course

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from courses.models import Course, Lesson
 
-# Create your views here.
+def index_courses(request):
+    courses = Course.objects.all()
+
+    return render(request, 'index_courses.html', {'courses': courses})
+
+
+def course_show(request, pk):
+    course = Course.objects.get(pk=pk)
+    course_lessons = course.lesson_set.all()
+
+    return render(request, 'course_show.html', {
+        'course': course,
+        'course_lessons': course_lessons,
+        })
