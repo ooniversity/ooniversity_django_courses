f05ad8556c6213d73bf7b989605f600b0c4fa1ad
Maxim Bilozorov <maximbilozorov@yandex.ua>
2015-04-07 15:13:41 +0300

    redo all + django=1.7.7 + remove instructors

--- instructors/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from instructors.models import Instructor
-
-def instructors_list(requst):
-	instructors = Instructor.objects.all()
-	return render(requst, "instructors_list.html", 
-					{'instructors':instructors})
