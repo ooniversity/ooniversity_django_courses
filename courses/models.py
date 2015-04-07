f2281317abb369d48bee38e2a34c2a62d542416a
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-07 14:53:47 +0300

    Add models for applications
    * For app courses
    * For app students

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField("Title", max_length=30)
+    descr_sm = models.CharField("Small description", max_length=128)
+    descr_full = models.TextField("Full description")
+
+
+    def __unicode__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    theme = models.CharField("Theme", max_length=64)
+    descr = models.TextField("Description")
+    course = models.ForeignKey(Course)
+    num_in_plan = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return self.theme
--- students/models.py
+++ students/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from courses.models import Course
+
+
+class Student(models.Model):
+    name = models.CharField("Name", max_length=25)
+    surname = models.CharField("Surname", max_length=25)
+    birthday = models.DateField("Birthday")
+    email = models.EmailField("Email", unique=True)
+    phone_num = models.CharField("Phone number", unique=True, max_length=12)
+    address = models.CharField("Address", max_length=256)
+    skype = models.CharField("Skype", max_length=128)
+    courses = models.ManyToManyField(Course)
