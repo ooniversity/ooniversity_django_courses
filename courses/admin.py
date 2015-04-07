9f9c480fb305dc1ff15fecfda7711b06d562ff1b
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-07 18:19:15 +0300

    Register our models in admin page
    * for courses
    * for students

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Course, Lesson
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'descr_sm']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['theme', 'num_in_plan', 'course']
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from students.models import Student
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'get_courses']
+
+admin.site.register(Student, StudentAdmin)
