6581e27dc74c62c5e8b2a5fcc518f4c5076215a1
Alexey <you@example.com>
2015-04-07 09:39:24 +0300

    New models are registrated

--- courses/admin.py
+++ courses/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from django.db import models
 from django.forms import widgets
 from courses.models import Course, Lesson
+
+
+admin.site.register(Course)
+admin.site.register(Lesson)
--- students/admin.py
+++ students/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from django.db import models
 from django.forms import widgets
 from students.models import Student
+
+
+admin.site.register(Student)
