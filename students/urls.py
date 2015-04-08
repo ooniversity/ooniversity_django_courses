ee32ddf219d26bf2109f17b469a52db306b61b3c
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-08 16:13:12 +0300

    Create new pages for students
    * Create urls
    * Create views
    * Create folder 'templates'
    * Create two files: detail_info.html (view detail info about each student); students_list.html (view list of all students)

---
+++ students/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+
+from students.views import students_list, student_detail
+
+urlpatterns = patterns('',
+    url(r'^$', students_list, name='students_list'),
+    url(r'^(?P<pk>\d+)/$', student_detail, name='student_detail'),
+)
