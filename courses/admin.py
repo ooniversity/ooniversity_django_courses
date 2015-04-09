361f42763fa648313ff61dfa7286f8f125299816
Andrey Sviridov <sviridoff@inbox.ru>
2015-04-09 19:50:11 +0300

    w5.2 some fixing

--- courses/admin.py
+++ courses/admin.py
@@ -3,6 +3,7 @@ from courses.models import Course, Lesson
 
 class LessonInline(admin.TabularInline):
     model = Lesson
+    extra = 3
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['name', 'short_description']
