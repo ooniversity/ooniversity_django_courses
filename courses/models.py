c78c8f39d6e4a9056bbcd49f2d40dbfa1cf4622d
Alexey <you@example.com>
2015-04-10 09:04:43 +0300

    Foreign keys with related_name

--- courses/models.py
+++ courses/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from coaches.models import Coach
 
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
     short_description = models.CharField(max_length=1000)
     description = models.TextField()
+    #instructor = models.ForeignKey(Coach, related_name='instructor')
+    #assistant = models.ForeignKey(Coach, related_name='assistant')
 
     def __unicode__(self):
         return self.name
