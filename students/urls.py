c001c4836af9fecb4919c2df957aadaf5d7997f0
andreyo <ao.mail@ua.fm>
2015-04-12 02:19:51 +0300

    task 5-1. not finished. stud fixed

--- students/urls.py
+++ students/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from students import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<course_id>\d+)/$', views.linked_students, name='linked_students'),
+    url(r'^$', views.index, name='index'),
+    #url(r'^(?P<course_id>\d+)/$', views.linked_students, name='linked_students'),
     url(r'^(?P<pk>\d+)/$', views.student_detail, name='detail'),
 )
--- students/views.py
+++ students/views.py
@@ -4,19 +4,13 @@ from django.views import generic
 from students.models import Student
 
 
-class IndexView(generic.ListView):
-    template_name = 'students/index.html'
-    context_object_name = 'student_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Student.objects.order_by('-id')[:5]
-
-def linked_students(request):
-    #p = get_object_or_404(Course, pk=course_id)
-    course = Course.objects.get(id=pk)
-    linked_students = Student.objects.filter(course=pk)
-    return render(request, 'students/index.html', {'course': course, 'lesson_list': linked_students})
+def index(request):
+    pk = request.GET.get('course_id')
+    if pk:
+        linked_students = Student.objects.filter(courses=int(pk))
+    else:
+        linked_students = Student.objects.all()
+    return render(request, 'students/index.html', {'student_list': linked_students})
 
 def student_detail(request):
     pass
