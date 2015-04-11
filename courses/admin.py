78ba095d4e28f5ab3621a36062c43c605cfe1a0a
nurekil <sasha_nurekil@mail.ru>
2015-04-11 18:48:22 +0300

    customized django administration

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
 
-admin.site.register(Course)
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    extra = 0
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'about']
+    list_display_links = ['title']
+    search_fields = ['title']
+    inlines = [LessonInline]
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
