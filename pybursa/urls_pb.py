c951e56dfd780fe474156ff47cfca32265a08aab
Олег Харюк <khfzar65@mail.ru>
2015-04-11 22:29:29 +0300

    KZ_5

---
+++ pybursa/urls_pb.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from pybursa.views import pybursa, pybursa_contact, pybursa_student_list, pybursa_student_detail
+ 
+
+urlpatterns = patterns('',
+    url(r'^$', pybursa, name='home'),
+    url(r'^contact/$', pybursa_contact, name='contact'),
+    url(r'^student_list/$', pybursa_student_list, name='student_list'),
+    url(r'^student_detail/$', pybursa_student_detail, name='student_detail'),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^quadratic/', include('quadratic.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
