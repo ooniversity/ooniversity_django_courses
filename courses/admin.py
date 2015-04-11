da11f4c22b4fcae9d182189cca9d5b7c750eec9e
Kostiantyn Moskalenko <con.mosk@gmail.com>
2015-04-12 00:19:07 +0300

    kz5.2

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
 
-admin.site.register(Course)
+
 admin.site.register(Lesson)
+
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title', 'descr']
+    list_editable = ['title']
+admin.site.register(Course, CourseAdmin)
--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
-
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['surname', 'email']
+    list_display = ['surname', 'email', 'skype']
+#    list_display = ['my_url_field', 'email', 'skype']
+#    def my_url_field(self, obj):
+#        return '<a href="%s%s">%s</a>' % ('surname', obj.url_field, obj.url_field)
+#    my_url_field.allow_tags = True
+#    my_url_field.short_description = 'Full name'
+#    raw_id_fields = ['']
+    list_filter = ['courses']
+admin.site.register(Student, StudentAdmin)
