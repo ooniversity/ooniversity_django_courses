e713fd236aa40a0ef78c0dc80e3371faa40f24fa
andreyo <ao.mail@ua.fm>
2015-04-13 02:06:26 +0300

    task 5-2. course customization - done.

--- courses/admin.py
+++ courses/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 from courses.models import Course, Lesson
 
 
-admin.site.register(Course)
+class LessonInline(admin.TabularInline):
+    model = Lesson
+
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ('title', 'comment')
+    inlines = [
+        LessonInline,
+    ]
+
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
 
