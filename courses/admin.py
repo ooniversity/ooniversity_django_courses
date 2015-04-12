b6bf60f4e749d952db0366ec59a272d6f183f6fe
Volodya Esk <vkhatianovskyi@gmail.com>
2015-04-12 15:50:33 +0300

    Adminka is configured

--- courses/admin.py
+++ courses/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 from courses.models import Course, Lesson
 
 
-admin.site.register(Course)
-admin.site.register(Lesson)
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['theme','description', 'order_number']
+
+
+
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    extra = 0
+
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name','short_description']
+    inlines = [LessonInline]
+    search_fields = ['name']
+
+
+
+    
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
 
