d563728beca673fe296b675896ba78798cf04a96
Lisa Gosteva <lisa.gosteva@gmail.com>
2015-04-13 21:25:38 +0300

    URl issues fixed

--- coaches/urls.py
+++ coaches/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 
 from coaches import views
 
-urlpatterns = patterns('coaches.views',
-    url(r'^/(?P<coach_id>\d+)/$', views.coach_d, name='coach_d'), 
+urlpatterns = patterns('',
+    url(r'^(?P<coach_id>\d+)/$', views.coach_d, name='coach_d'), 
 )
+
+
