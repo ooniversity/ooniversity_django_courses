64bdd245a2a7a087aa23e65ee0c06d0fe9d35582
Oksana Slusarenko <oksana.slu@gmail.com>
2015-04-13 09:27:45 +0300

    added QuadraticForm

---
+++ quadratic/forms.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+
+class QuadraticForm(forms.Form):
+    a = forms.IntegerField(label="коэффициент a")
+    b = forms.IntegerField(label="коэффициент b")
+    c = forms.IntegerField(label="коэффициент c")
+
+    def clean_a(self):
+        data = self.cleaned_data['a']
+        if data == 0:
+            raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
+        return data
--- quadratic/views.py
+++ quadratic/views.py
@@ -1,30 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-
-
-class Coefficient(object):
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-        self.value_int = None
-        self.error_message = None
-
-    def is_valid(self):
-        if not self.value:
-            self.error_message = "коэффициент не определен"
-            return False
-
-        try:
-            self.value_int = int(self.value)
-        except ValueError:
-            self.error_message = "коэффициент не целое число"
-            return False
-
-        if self.name == 'a' and self.value_int == 0:
-            self.error_message = "коэффициент при первом слагаемом уравнения не может быть равным нулю"
-            return False
-        return True
+from quadratic.forms import QuadraticForm
 
 
 def get_discr(a, b, c):
@@ -41,29 +17,23 @@ def get_eq_root(a, b, d, order=1):
 
 
 def quadratic_results(request):
-    context = {'error': False}
-    for name_value in ['a', 'b', 'c']:
-        coefficient = Coefficient(name_value, request.GET.get(name_value, ''))
-        if coefficient.is_valid():
-            context[name_value] = coefficient.value_int
-        else:
-            context['error'] = True
-            context[name_value + '_error'] = coefficient.error_message
-            context[name_value] = coefficient.value
-    if not context['error']:
-        a = context['a']
-        b = context['b']
-        c = context['c']
-        d = get_discr(a, b, c)
-        if d < 0:
-            result_message = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-        elif d == 0:
-            x = get_eq_root(a, b, d)
-            result_message = "Дискриминант равен нулю, квадратное уравнение имеет один действительных корень: x1 = x2 = {}".format(x)
-        else:
-            x1 = get_eq_root(a, b, d)
-            x2 = get_eq_root(a, b, d, order=2)
-            result_message = "Квадратное уравнение имеет два действительных корня: x1 = {}, x2 = {}".format(x1, x2)
-
-        context.update({'d': d, 'result_message': result_message})
+    context = {}
+    if request.GET:
+        form = QuadraticForm(request.GET)
+        if form.is_valid():
+            data = form.cleaned_data
+            d = get_discr(**data)
+            if d < 0:
+                result_message = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+            elif d == 0:
+                x = get_eq_root(data['a'], data['b'], d)
+                result_message = "Дискриминант равен нулю, квадратное уравнение имеет один действительных корень: x1 = x2 = {}".format(x)
+            else:
+                x1 = get_eq_root(data['a'], data['b'], d)
+                x2 = get_eq_root(data['a'], data['b'], d, order=2)
+                result_message = "Квадратное уравнение имеет два действительных корня: x1 = {}, x2 = {}".format(x1, x2)
+            context.update({'d': d, 'result_message': result_message})
+    else:
+        form = QuadraticForm()
+    context.update({'form': form})
     return render(request, 'quadratic/results.html', context)
