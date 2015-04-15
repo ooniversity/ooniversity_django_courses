c78704fab93b2381e753b7d2d477eff24eca9732
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2015-04-15 10:57:00 +0300

    Task 7.2 almost OK

--- quadratic/utils/utils.py
+++ quadratic/utils/utils.py
@@ -48,22 +48,22 @@ def get_eq_root(a, b, d, order=1):
 def quadratic_equel (par1, par2, par3):
     dict_result = {'a':'none', 'b':'none', 'c':'none', 'd':'', 'x_1':'none', 'x_2':'none', 'result':'none'}
     #Определение коефициентов
-    dict_result['a'] = check_param(par1)
-    dict_result['b'] = check_param(par2)
-    dict_result['c'] = check_param(par3)
+    dict_result['a'] = par1
+    dict_result['b'] = par2
+    dict_result['c'] = par3
     if dict_result['a'] == 0:
         dict_result['a'] = 'first param is zero'
     #Решение квадратного уравнения
-    if type(dict_result['a']) is int and type(dict_result['b']) is int and type(dict_result['c']) is int:
-        dict_result['d'] = get_discr(dict_result['a'], dict_result['b'], dict_result['c'])
-        if dict_result['d'] <0:
-            dict_result['result'] = 'Disct less zero'
-        elif dict_result['d']==0:
-            dict_result['x_1'] = get_eq_root(dict_result['a'], dict_result['b'], dict_result['d'])
-            dict_result['x_2'] = dict_result['x_1']
-            dict_result['result'] = 'One root'
-        else:
-            dict_result['x_1'] = get_eq_root(dict_result['a'], dict_result['b'], dict_result['d'])
-            dict_result['x_2'] = get_eq_root(dict_result['a'], dict_result['b'], dict_result['d'], 0)
-            dict_result['result'] = 'Two roots'
+    #if type(dict_result['a']) is int and type(dict_result['b']) is int and type(dict_result['c']) is int:
+    dict_result['d'] = int(get_discr(dict_result['a'], dict_result['b'], dict_result['c']))
+    if dict_result['d'] <0:
+        dict_result['result'] = 'Disct less zero'
+    elif dict_result['d']==0:
+        dict_result['x_1'] = get_eq_root(dict_result['a'], dict_result['b'], dict_result['d'])
+        dict_result['x_2'] = dict_result['x_1']
+        dict_result['result'] = 'One root'
+    else:
+        dict_result['x_1'] = get_eq_root(dict_result['a'], dict_result['b'], dict_result['d'])
+        dict_result['x_2'] = get_eq_root(dict_result['a'], dict_result['b'], dict_result['d'], 0)
+        dict_result['result'] = 'Two roots'
     return dict_result
--- quadratic/views.py
+++ quadratic/views.py
@@ -1,16 +1,48 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from utils import utils
+from django import forms
+from django.core import validators
 
 # Create your views here.
-from utils import utils
+
+#Создаем класс, который будет формой для задания коефициентов квадратного уравнения
+class QuadraticForm(forms.Form):
+    a = forms.FloatField(required=True, label='коеффициент а')
+    b = forms.FloatField(required=True, label='коеффициент b')
+    c = forms.FloatField(required=True, label='коеффициент c')
+
+    def valid_a(self, var):
+        data = var
+        if data == 0:
+            raise forms.ValidationError("You have forgotten about Fred!")
+        # Always return the cleaned data, whether you have changed it or
+        # not.
+        return data
 
 
 def quadratic_results (request):
-    #Вызываем функция quadratic_equel, которая посчитает квадратное уравнение
-    #Функция возвращаем словать со всеми значениями, которые нужно будет передать на WEB
-    #Мне показалось, что так удобней
-    dict_quadr = utils.quadratic_equel(request.GET['a'], request.GET['b'], request.GET['c'])
+    #Инстанцируем форму
+    form = QuadraticForm()
+    #Делаем так, чтобы значение после GET-запроса(нажали на Решить)
+    #не исчезали, а оставались в полях. Чтобы пользователь не вводил данные много раз
+    #есть данные были не корректные
+    if request.method == "GET":
+        form = QuadraticForm(request.GET)
+        #Проверка данных на правильность (валидность)
+        if form.is_valid():
+            form.valid_a(form.cleaned_data['a'])
+            #print form.cleaned_data Джанго сохраняет очищенные данные в переменной cleaned_data
+            #Вызываем функция quadratic_equel, которая посчитает квадратное уравнение
+            #Функция возвращаем словать со всеми значениями, которые нужно будет передать на WEB
+            #Мне показалось, что так удобне
+            dict_quadr = utils.quadratic_equel(form.cleaned_data['a'], form.cleaned_data['b'], form.cleaned_data['c'])
+        else:
+            dict_quadr = {}
+    else:
+        form = QuadraticForm(request.POST)
 
-    #Парсим HTML страничку и передаем ей все необходимие переменные
-    return render (request, 'quadratic/index_quadratic.html',
-        {'a_native':request.GET['a'], 'b_native':request.GET['b'], 'c_native':request.GET['c'],  'diction':dict_quadr},)
+    #context = {'a_native':request.GET.get('a'), 'b_native':request.GET.get('b'), 'c_native':request.GET.get('c'), }
+    context = {'diction':dict_quadr, 'form':form, }
+    #Отображаем HTML страничку и передаем ей все необходимие переменные
+    return render (request, 'quadratic/index_quadratic.html', context)
