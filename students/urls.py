42573b7a20ae0dc8c4c3f539c4c6944a29f5e940
Alexey <you@example.com>
2015-04-15 09:40:13 +0300

    Base_person_add.html added

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from students.views import student_list, student_detail
+from students.views import student_list, student_detail, student_add
 
 
 urlpatterns = patterns('',
     url(r'^$', student_list, name='student_list'),
     url(r'^(?P<student_id>\d+)/$', student_detail, name='student_detail'),
+    url(r'^add/$', student_add, name='student_add'),
 )
--- students/views.py
+++ students/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django import forms
 from students.models import Student
 
 
@@ -15,4 +16,18 @@ def student_detail(request, student_id):
     student = Student.objects.get(id=student_id)
     courses = student.courses.all()
     return render(request, 'students/student_detail.html', {'student': student, 'courses': courses})
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+
+
+
+def student_add(request):
+    model_form = StudentForm()
+    """
+Model forms, 2 video, 2:55
+    """
+    return render(request, 'students/student_add.html', {'model_form': model_form})
 
