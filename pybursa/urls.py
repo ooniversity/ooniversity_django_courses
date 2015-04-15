010e93f0253212a57f457ba06248eb9b26c53a9e
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-15 18:09:31 +0300

    Delete unused urls

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -7,8 +7,6 @@ urlpatterns = patterns(
     '',
     url(r'^$', index_courses, name='home'),
     url(r'^contact/$', contact, name='contact'),
-    # url(r'^student_list/$', student_list, name='student_list'),
-    # url(r'^student_detail/$', student_detail, name='student_detail'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^quadratic/', include('quadratic.urls', namespace='quadratic')),
