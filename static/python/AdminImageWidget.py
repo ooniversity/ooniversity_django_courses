8c9d58da1cb725abd775858794f4d63eafb14e48
Yaroslav Juvanov <pyromanser@mail.ru>
2015-04-15 11:41:28 +0300

    Made changes in image widget.

--- static/python/AdminImageWidget.py
+++ static/python/AdminImageWidget.py
@@ -1,10 +1,5 @@
 from django.contrib.admin.widgets import AdminFileWidget
-from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
-
-
-import os
-import Image
 
 
 class AdminImageWidget(AdminFileWidget):
@@ -12,38 +7,8 @@ class AdminImageWidget(AdminFileWidget):
         output = []
         if value and getattr(value, "url", None):
 
-            image_url = value.url
-            file_name=str(value)
+            output.append(u' <div><a href="%s"><img src="%s" height="100" /></a></div> ' % \
+                (value.url,value.url))
 
-            # defining the size
-            size='100x100'
-            x, y = [int(x) for x in size.split('x')]
-            try :
-                # defining the filename and the miniature filename
-                filehead, filetail  = os.path.split(value.path)
-                basename, format        = os.path.splitext(filetail)
-                miniature                   = basename + '_' + size + format
-                filename                        = value.path
-                miniature_filename  = os.path.join(filehead, miniature)
-                filehead, filetail  = os.path.split(value.url)
-                miniature_url           = filehead + '/' + miniature
-
-                # make sure that the thumbnail is a version of the current original sized image
-                if os.path.exists(miniature_filename) and os.path.getmtime(filename) > os.path.getmtime(miniature_filename):
-                    os.unlink(miniature_filename)
-
-                # if the image wasn't already resized, resize it
-                if not os.path.exists(miniature_filename):
-                    image = Image.open(filename)
-                    image.thumbnail([x, y], Image.ANTIALIAS)
-                    try:
-                        image.save(miniature_filename, image.format, quality=100, optimize=1)
-                    except:
-                        image.save(miniature_filename, image.format, quality=100)
-
-                output.append(u' <div><a href="%s" target="_blank"><img src="%s" alt="%s" /></a></div> ' % \
-                (miniature_url, miniature_url, miniature_filename))
-            except:
-                pass
         output.append(super(AdminFileWidget, self).render(name, value, attrs))
         return mark_safe(u''.join(output))
