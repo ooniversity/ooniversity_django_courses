d2b5ab930859f175e1ba8fe703b3fdedf0673bff
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-16 23:42:44 +0300

    Add URL on page with create new student

--- students/urls.py
+++ students/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 
-from students.views import students_list, student_detail
+from students.views import students_list, student_detail, student_add
 
 urlpatterns = patterns(
     '',
-    url(r'^$', students_list, name='students_list'),
-    url(r'^(?P<pk>\d+)/$', student_detail, name='student_detail'),
+    url(r'^$', students_list, name='students-list'),
+    url(r'^(?P<pk>\d+)/$', student_detail, name='student-detail'),
+    url(r'^add/$', student_add, name="student-add"),
 )
