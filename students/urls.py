c3eda9b3486ea255951306920425aba5aa80d82a
Paha <paha@ubuntu.ubuntu-domain>
2015-04-18 01:44:35 +0300

    Ceate pages to add, edit, remove student

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
 
-from students.views import students_all, student_info
+from students import views
 
 urlpatterns = patterns('',
-    url(r'^$', students_all, name='students_all'),
-    url(r'^(?P<student_id>\d+)/$', student_info, name='student_info'),
+    url(r'^$', views.students_all, name='students_all'),
+    url(r'^(?P<student_id>\d+)/$', views.student_info, name='student_info'),
+    url(r'^add/$', views.student_add, name='student_add'),
+    url(r'^edit/(?P<student_id>\d+)/$', views.student_edit, name='student_edit'),
+    url(r'^remove/(?P<student_id>\d+)/$', views.student_remove, name='student_remove'),
 )
