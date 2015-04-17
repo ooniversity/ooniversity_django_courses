c29e53ec1468f38a98c03ef53f0ae04110b96518
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-18 00:09:28 +0300

    Add URL's for courses pages
    * URL to edit page
    * URL to create page
    * URL to delete page

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-from courses.views import course_show
+from courses.views import course_show, course_delete, course_edit, course_add
 
 urlpatterns = patterns(
     '',
     url(r'^(?P<pk>\d+)/$', course_show, name='show'),
+    url(r'^add/$', course_add, name="course-add"),
+    url(r'^edit/(?P<pk>\d+)/$', course_edit, name="course-edit"),
+    url(r'^remove/(?P<pk>\d+)/$', course_delete, name="course-delete"),
+
 )
