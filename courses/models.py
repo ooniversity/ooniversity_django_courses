4b5d2873f67c0a9156aefb8c8e27abfcf724b307
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-18 00:10:52 +0300

    Create forms
    * Create CourseForm
    * Create LessonForm

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.forms import ModelForm, widgets
+from django import forms
+
 from coaches.models import Coach
 
 
@@ -19,6 +22,20 @@ class Course(models.Model):
         return self.title
 
 
+class CourseForm(ModelForm):
+    class Meta:
+        model = Course
+        widgets = {
+            'coach': forms.Select,
+            'assistant': forms.Select,
+        }
+        labels = {
+            'descr_sm': 'Description short',
+            'descr_full': 'Description full',
+        }
+        fields = '__all__'
+
+
 class Lesson(models.Model):
     theme = models.CharField("Theme", max_length=64)
     descr = models.TextField("Description")
@@ -27,3 +44,17 @@ class Lesson(models.Model):
 
     def __unicode__(self):
         return self.theme
+
+
+class LessonForm(ModelForm):
+    class Meta:
+        model = Lesson
+        widgets = {
+            'course': forms.SelectMultiple,
+        }
+        labels = {
+            'descr': 'Description',
+            'num_in_plan': 'Number in plan'
+        }
+        fields = '__all__'
+
