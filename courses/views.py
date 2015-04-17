c6c845d889853c7cbb0cb1bcd6e7c193927443c5
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-18 00:11:51 +0300

    Create function for delete, create and edit each course

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from courses.models import Course, Lesson
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.http import Http404
+
+from courses.models import Course, Lesson, CourseForm, LessonForm
 
 
 def index_courses(request):
@@ -34,3 +37,56 @@ def course_show(request, pk):
         'coach_info': coach_info,
         'assistant_info': assistant_info,
         })
+
+
+def course_delete(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+
+    if request.method == "POST":
+        course.delete()
+        messages.success(
+            request, u"Course: {} was deleted".format(
+                course.title))
+        return redirect('home')
+
+    return render(request, 'course_delete.html', {
+        'course': course,
+    })
+
+
+def course_add(request):
+    if request.method == "POST":
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            course = form.save()
+            messages.success(
+                request, u"Course {} add success!".format(
+                    course.title))
+            return redirect('home')
+    else:
+        form = CourseForm()
+
+    return render(request, 'course_add.html', {
+        'form': form,
+    })
+
+
+def course_edit(request, pk):
+    course = Course.objects.get(id=pk)
+
+    if request.method == "POST":
+        form = CourseForm(request.POST, instance=course)
+        if form.is_valid():
+            course.save()
+            messages.success(
+                request, u"{} update success!".format(
+                    course.title))
+            return redirect('home')
+    else:
+        form = CourseForm(instance=course)
+
+    return render(request, 'course_edit.html', {
+        'form': form,
+    })
+
+
