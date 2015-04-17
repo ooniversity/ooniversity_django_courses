8eecafcc7b5a9ef418b716d7c34811a3cf66d074
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-17 21:49:28 +0300

    Add URL's to edit and delete pages

--- students/urls.py
+++ students/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 
-from students.views import students_list, student_detail, student_add
+from students.views import (
+    students_list, student_detail, student_add, student_edit, student_delete)
 
 urlpatterns = patterns(
     '',
     url(r'^$', students_list, name='students-list'),
     url(r'^(?P<pk>\d+)/$', student_detail, name='student-detail'),
     url(r'^add/$', student_add, name="student-add"),
+    url(r'^edit/(?P<pk>\d+)/$', student_edit, name="student-edit"),
+    url(r'^remove/(?P<pk>\d+)/$', student_delete, name="student-delete"),
 )
