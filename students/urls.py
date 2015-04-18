482527f3a30ea4a935ee5fde7985a6b427ccbbfb
Lisa Gosteva <lisa.gosteva@gmail.com>
2015-04-18 23:02:59 +0300

    Edit student template, view, urls

--- students/urls.py
+++ students/urls.py
@@ -5,6 +5,7 @@ from students import views
 urlpatterns = patterns('',
     url(r'^/$', views.students, name='students'),
     url(r'/(?P<pk>\d+)/$', views.student_d, name='student_d'),
-    url(r'add/$', views.add_stundent, name='add_stundent'), 
+    url(r'/add/$', views.add_stundent, name='add_stundent'),
+    url(r'/edit/$', views.edit_student, name='edit_student'), 
 )
 
--- students/views.py
+++ students/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+# -*- coding: utf-8 -*-
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
+from django.contrib import messages
+from django import forms 
 
 from courses.models import Course
 from models import Student
+
+class StudentFrom(forms.ModelForm):
+    class Meta:
+        model = Student
+
 
 def students(request):
     course_id = request.GET.get('course_id', None)
@@ -17,4 +25,16 @@ def student_d(request, pk):
     return render(request, 'students/student_detail.html', {'student': student})
 
 def add_stundent(request):
-    return render(request,'students/add.html')
+    if request.method == 'POST':
+        form = StudentFrom(request.POST)
+        if form.is_valid():
+            new_student = form.save()
+            student = Student.objects.get(id=9)
+            messages.success(request, 'Студент успешно добавлен', {student:student})
+            return redirect('students:students')
+    else:
+        form = StudentFrom()
+    return render(request,'students/add.html', {'form':form})
+
+def edit_student(request):
+    return render(request,'students/edit.html')
