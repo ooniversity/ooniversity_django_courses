8a2f6cd9baf1b2ee73294a4205e4eb41d1394923
Andrey Sviridov <sviridoff@inbox.ru>
2015-04-18 21:39:57 +0300

    7.2

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,40 +1,39 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
 
-def checktype(var):
-    try:
-        int(var)
-    except ValueError:
-       var = [var, 'stringtype']
-    return var
+
+class QuadraticForm(forms.Form):
+    a = forms.IntegerField(required=True, label='коээфициент a:')
+    b = forms.IntegerField(required=True, label='коээфициент b:')
+    c = forms.IntegerField(required=True, label='коээфициент c:')
+
+    def clean_a(self):
+        a_data = self.cleaned_data['a']
+        if a_data == 0:
+            raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю.")
+        return a_data
 
 def quadratic_results(request):
-    dictionary = {'a': '', 'b': '', 'c': '', 'nd': 'коэффициент не определён',
-            'ni': 'коэффициент не целое число'}
-    for i in dictionary:
-        if i in request.GET:
-            dictionary[i] = request.GET.get(i)
-            if dictionary[i] != '':
-                dictionary[i] = checktype(dictionary[i])
-
-    if dictionary['a'] != '0':
-        for i in dictionary:
-            if dictionary[i] != '' and type(dictionary[i]) is not list:
-                pass
-            else:
-                return render(request, 'quadratic.html', dictionary)
-    else:
-        return render(request, 'quadratic.html', dictionary)
-    discr = float(dictionary['b']) ** 2 - 4 * float(dictionary['a']) * float(dictionary['c'])
-    x1 = 0
-    x2 = 0
-    if discr == 0:
-        x1 = x2 = -float(dictionary['b']) / 2*float(dictionary['a'])
-    elif discr > 0:
-        x1 = (-float(dictionary['b']) + discr ** 0.5) / (2 * float(dictionary['a']))
-        x2 = (-float(dictionary['b']) - discr ** 0.5) / (2 * float(dictionary['a']))
-    dictionary['discr'] = int(discr)
-    dictionary['x1'] = x1
-    dictionary['x2'] = x2
-    return render(request, 'quadratic.html', dictionary)
+    form = QuadraticForm()
+    data = {}
+    if request.GET:
+        form = QuadraticForm(request.GET)
+        if form.is_valid():
+            data = form.cleaned_data
+            discr = data['b'] ** 2 - 4 * data['a'] * data['c']
+            print discr
+            x1 = 0
+            x2 = 0
+            if discr == 0:
+                x1 = x2 = -float(data['b']) / 2 * data['a']
+            elif discr > 0:
+                x1 = float(-data['b'] + discr ** 0.5) / (2 * data['a'])
+                x2 = float(-data['b'] - discr ** 0.5) / (2 * data['a'])
+            data['x1'] = x1
+            data['x2'] = x2
+            data['discr'] = discr
+            data['form'] = form
+    context = {'data': data, 'form': form}
+    return render(request, 'quadratic.html', context)
