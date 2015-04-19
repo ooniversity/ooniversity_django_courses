4de8f7ad06ef797c6f3ef89c5f5a685090f33027
Oleg <oleg_ishchenko@i.ua>
2015-04-19 21:43:49 +0300

    Quadratic equation

--- quadratic/views.py
+++ quadratic/views.py
@@ -1,27 +1,39 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
-
 from quadratic.models import *
+from django import forms
+
+class QuadraticForm(forms.Form):
+    a = forms.FloatField(label=u"Коэффициент a:")
+    b = forms.FloatField(label=u"Коэффициент b:")
+    c = forms.FloatField(label=u"Коэффициент c:")
+
+    def clean_a(a):
+        a = a.cleaned_data['a']#'a'
+        if float(a) == 0:
+            raise forms.ValidationError(u"Коэффициент при первом слагаемом уравнения не может быть равным нулю")
+        return a
 
 def parameters(request):
+    context = {}
+    errors = 0
 
-    a = request.GET['a']
-    b = request.GET['b']
-    c = request.GET['c']
+    if request.GET:
+        form = QuadraticForm(request.GET)
+        if form.is_valid():
+            a = form.cleaned_data.get('a')
+            b = form.cleaned_data.get('b')
+            c = form.cleaned_data.get('c')
 
-    zero_a = find_zero(a)
+            d = get_discr(a, b, c)
+            context['d'] = d
+            eq_root = get_eq_root(a, b, d)
+            context['eq_root'] = eq_root               
+        
+    else:
+        form = QuadraticForm()
 
-    except_a = find_exception(a)
-    except_b = find_exception(b)
-    except_c = find_exception(c)
+    context['form'] = form         
 
-    d = get_discr(a, b, c)
-
-    eq_root = get_eq_root(a, b, d)
-
-    return render (request, 'results.html',
-                   {'a':a, 'b': b, 'c':c, 'd':d, 'zero_a': zero_a,
-                    'except_a':except_a, 'except_b':except_b, 'except_c':except_c,
-                    'eq_root': eq_root}  )
-
-
+    return render (request, 'results.html',context  )
