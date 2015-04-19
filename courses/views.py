e2c237a292435a3a5c28f151a20599a3320d236f
sergei4e <4e.sergei@gmail.com>
2015-04-20 01:54:51 +0300

    Задание 8 выполнено

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+# -*- coding: utf_8 -*-
+from django.shortcuts import render, redirect
+from django.forms import ModelForm
+from django.contrib import messages
 from courses.models import Courses, Lesson
+
+
+class CoursesForm(ModelForm):
+    class Meta:
+        model = Courses
+
+
+class LessonForm(ModelForm):
+    class Meta:
+        model = Lesson
 
 
 def index(request):
@@ -9,8 +22,60 @@ def index(request):
 
 
 def course_view(request, pk):
-    one_c = Courses.objects.get(id__exact=int(pk))
-    lessons = Lesson.objects.filter(course__id__exact=int(pk))
-    print one_c.trener.desc
-    c = {"course": one_c, "lessons": lessons}
+    course = Courses.objects.get(pk=pk)
+    lessons = course.lesson_set.all()
+    c = {"course": course, "lessons": lessons}
     return render(request, 'course.html', c)
+
+
+def course_add(request):
+    if request.method == 'POST':
+        form = CoursesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            course = form.cleaned_data['name']
+            messages.success(request, (u'Новый курс %s был добавлен' % course))
+            return redirect('home')
+    else:
+        form = CoursesForm()
+    context = {'form': form}
+    return render(request, 'course_add.html', context)
+
+
+def course_edit(request, pk):
+    course = Courses.objects.get(id=int(pk))
+    form = CoursesForm(instance=course)
+    if request.method == 'POST':
+        form = CoursesForm(request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            messages.success(request, (u'Информация о курсе %s обновлена' % course.name))
+            return redirect('home')
+    context = {'form': form,
+               'course': course}
+    return render(request, 'course_edit.html', context)
+
+
+def course_delete(request, pk):
+    course = Courses.objects.get(id=int(pk))
+    if request.method == 'POST':
+        course.delete()
+        messages.success(request, (u'Курс %s был удален' % course.name))
+        return redirect('home')
+    context = {'course': course}
+    return render(request, 'course_delete.html', context)
+
+
+def add_lesson(request, pk):
+    course = Courses.objects.get(pk=pk)
+    form = LessonForm(initial={'course': course})
+    if request.method == 'POST':
+        form = LessonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, (u'Занятие %s было добавлено на курс %s' %
+                                       (form.cleaned_data['thema'], course.name)))
+            return redirect('course', pk)
+    context = {'form': form,
+               'course': course}
+    return render(request, 'add_lesson.html', context)
