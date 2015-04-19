2a3b11fa9d2cf14cc891fb17358a8b4f1125d65d
andreyo <ao.mail@ua.fm>
2015-04-19 23:20:31 +0300

    task 8. lesson - form added. done

--- courses/CourseForm.py
+++ courses/CourseForm.py
@@ -1,9 +1,14 @@
 # coding=utf-8
 from django import forms
 
-from courses.models import Course
+from courses.models import Course, Lesson
 
 
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
+
+
+class LessonForm(forms.ModelForm):
+    class Meta:
+        model = Lesson
--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 from courses import views
+from courses import views_lesson
 
 
 urlpatterns = patterns('',
@@ -9,4 +10,6 @@ urlpatterns = patterns('',
     url(r'^add/', views.course_add, name='add'),
     url(r'^edit/(?P<pk>\d+)/$', views.course_edit, name='edit'),
     url(r'^remove/(?P<pk>\d+)/$', views.course_remove, name='remove'),
+    # lessons
+    url(r'^(?P<pk>\d+)/add_lesson/', views_lesson.item_add, name='add_lesson'),
 )
