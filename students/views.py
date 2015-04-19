4396a1d2cc12c5c0fda5f106e00d2853685a6993
andreyo <ao.mail@ua.fm>
2015-04-19 16:25:13 +0300

    task 7-3. student - edit

--- students/views.py
+++ students/views.py
@@ -13,31 +13,52 @@ def index(request):
         linked_students = Student.objects.filter(courses=int(pk))
     else:
         linked_students = Student.objects.all()
+
     return render(request, 'students/index.html', {'student_list': linked_students})
+
 
 def student_detail(request, pk):
     student = Student.objects.get(id=pk)
+
     return render(request, 'students/detail.html', {'student': student})
 
+
 def student_add(request):
-    #student = {}
     if request.method == 'POST':
         form = StudentForm(request.POST)
         if form.is_valid():
             new_student = form.save()
-            add_message = 'Студент ' + ' успешно добавлен'
-            messages.success(request, add_message)
+            info_message = 'Студент ' + ' успешно добавлен'
+            messages.success(request, info_message)
             return redirect('students:index')
     else:
         form = StudentForm()
+    action_name = "   Создать   "
+
     return render(request, 'students/add.html', {
-        #'student': student,
-        'form': form,})
+        'form': form,
+        'action_name': action_name,})
+
 
 def student_edit(request, pk):
     student = Student.objects.get(id=pk)
-    return render(request, 'students/detail.html', {'student': student})
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            new_student = form.save()
+            info_message = 'Данные изменены.'
+            messages.success(request, info_message)
+            return redirect('students:edit', pk=pk)
+    else:
+        form = StudentForm(instance=student)
+    action_name = "   Изменить   "
+
+    return render(request, 'students/edit.html', {
+        'form': form,
+        'action_name': action_name,})
+
 
 def student_remove(request, pk):
     student = Student.objects.get(id=pk)
+
     return render(request, 'students/detail.html', {'student': student})
