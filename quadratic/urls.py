81912e08f8bea9d03c3a02f6f9dc9388346e0b32
Oleg <oleg_ishchenko@i.ua>
2015-04-19 22:34:19 +0300

    Changed students

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from views import *
 
+
 urlpatterns = patterns('',
     url(r'^results/$', parameters, name='result'),
 )
--- quadratic/views.py
+++ quadratic/views.py
@@ -4,10 +4,23 @@ from django.shortcuts import render
 from quadratic.models import *
 from django import forms
 
+<<<<<<< HEAD
+
+=======
+>>>>>>> 4de8f7ad06ef797c6f3ef89c5f5a685090f33027
 class QuadraticForm(forms.Form):
     a = forms.FloatField(label=u"Коэффициент a:")
     b = forms.FloatField(label=u"Коэффициент b:")
     c = forms.FloatField(label=u"Коэффициент c:")
+<<<<<<< HEAD
+
+    def clean_a(a):
+        a = a.cleaned_data['a']#'a'
+        if float(a) == 0:
+            raise forms.ValidationError(u"Коэффициент при первом слагаемом уравнения не может быть равным нулю")
+        return a
+=======
+>>>>>>> 4de8f7ad06ef797c6f3ef89c5f5a685090f33027
 
     def clean_a(a):
         a = a.cleaned_data['a']#'a'
@@ -29,6 +42,14 @@ def parameters(request):
             d = get_discr(a, b, c)
             context['d'] = d
             eq_root = get_eq_root(a, b, d)
+<<<<<<< HEAD
+            context['eq_root'] = eq_root       
+    else:
+        form = QuadraticForm()
+
+    context['form'] = form
+    
+=======
             context['eq_root'] = eq_root               
         
     else:
@@ -36,4 +57,5 @@ def parameters(request):
 
     context['form'] = form         
 
+>>>>>>> 4de8f7ad06ef797c6f3ef89c5f5a685090f33027
     return render (request, 'results.html',context  )
