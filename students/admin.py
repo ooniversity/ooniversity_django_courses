4ec6d66f407e67cda54187586d5a2cafed03d488
Karakash Sergey <karakash.sergey@gmail.com>
2015-04-19 23:25:22 +0300

    task 8

--- students/admin.py
+++ students/admin.py
@@ -11,15 +11,8 @@ class StudentAdmin(admin.ModelAdmin):
     ]
 
 
-"""class StudentAddAdmin(admin.ModelAdmin):
-    list_display = ['name_surname', 'email', 'skype']
-    list_filter = ['course']
-    filter_horizontal = ['course']
-    fieldsets = [
-        ('Personal info',{'fields': ['name', 'surname', 'date_of_birth']}),
-        ('Contact info', {'fields': ['email', 'phone', 'adress', 'skype', 'course']}),
-    ]"""
+
 admin.site.register(Student, StudentAdmin)
-"""admin.site.register(StudentAdd,StudentAddAdmin)"""
+
 
 
--- students/models.py
+++ students/models.py
@@ -17,20 +17,3 @@ class Student(models.Model):
     def __unicode__(self):
         return self.name_surname()
 
-"""
-class StudentAdd(models.Model):
-    name = models.CharField(max_length=255)
-    surname = models.CharField(max_length=255)
-    date_of_birth = models.DateField()
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
-    adress = models.CharField(max_length=255)
-    skype = models.CharField(max_length=255)
-    course = models.ManyToManyField(Course)
-
-    def name_surname(self):
-        return self.name + ' ' + self.surname
-
-    def __unicode__(self):
-        return self.name
-"""
