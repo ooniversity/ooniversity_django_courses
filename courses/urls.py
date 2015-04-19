4a3504aafd293e1c8f87c8e7aee03c1863d05542
Volodya Esk <vkhatianovskyi@gmail.com>
2015-04-19 21:22:24 +0300

    First part of 8th task is done

--- courses/urls.py
+++ courses/urls.py
@@ -3,6 +3,8 @@ from courses import views
 
 
 urlpatterns = patterns('',
-   url(r'(?P<c_id>\d+)/$', views.course, name='courses'),
-
+    url(r'^(?P<c_id>\d+)/$', views.course, name='courses'),
+    url(r'^add/$', views.add_course, name='add_course'),
+    url(r'^edit/(?P<id>\d+)/$', views.edit_course, name='edit_course'),
+    url(r'^remove/(?P<id>\d+)$', views.remove_course, name='remove_course'),
 )
