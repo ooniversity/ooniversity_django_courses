209bfecc21c7e689abbef86daa65260ea5c5457e
Олег Харюк <khfzar65@mail.ru>
2015-04-19 23:28:37 +0300

    KZ8

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from courses.views import course, lesson_pd, lesson_js, lesson_rr
+from courses.views import course, lesson_pd, lesson_js, lesson_rr, add_lesson
 from students.views import student
 
  
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^courses/2/$', lesson_js, name='2'),
     url(r'^courses/3/$', lesson_rr, name='3'),
     url(r'^students/$', student, name='student'),
+    url(r'^add_lesson/$', add_lesson, name='add_lesson'),
     
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^quadratic/', include('quadratic.urls')),
