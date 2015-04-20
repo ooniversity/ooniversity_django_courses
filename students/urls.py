e910aad8a130d6fdcca8cec3ad657047141519cd
Alexey <you@example.com>
2015-04-20 22:05:47 +0300

    DeleteView for students added

--- students/urls.py
+++ students/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from students.views import StudentsListView, StudentDetailView, StudentCreateView, StudentUpdateView, student_remove
+from students.views import StudentsListView, StudentDetailView, StudentCreateView, StudentUpdateView, StudentDeleteView
 
 
 urlpatterns = patterns('',
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/$', StudentDetailView.as_view(), name='student_detail'),
     url(r'^add/$', StudentCreateView.as_view(), name='student_add'),
     url(r'^edit/(?P<pk>\d+)/$', StudentUpdateView.as_view(), name='student_edit'),
-    url(r'^remove/(?P<student_id>\d+)/$', student_remove, name='student_remove'),
+    url(r'^remove/(?P<pk>\d+)/$', StudentDeleteView.as_view(), name='student_remove'),
 )
--- students/views.py
+++ students/views.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
-from django.shortcuts import render, redirect, get_object_or_404
-from django import forms
 from django.contrib import messages
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from students.models import Student
 
@@ -60,41 +58,20 @@ class StudentUpdateView(UpdateView):
                           .format(datetime.now().strftime("%H:%M:%S")))
         return form
 
-class StudentForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = '__all__'
 
+class StudentDeleteView(DeleteView):
+    model = Student
+    success_url = reverse_lazy('students:student_list')
 
-'''
-def student_edit(request, student_id):
-    page_title = u"Редактирование данных студента"
-    student = get_object_or_404(Student, pk=student_id)
-    if request.method == 'POST':
-        form = StudentForm(request.POST, instance=student)
-        if form.is_valid():
-            student = form.save()
-            messages.success(request, 
-                            u"Изменения данных студента сохранены в {0}"\
-                             .format(datetime.now().strftime("%H:%M:%S")))
-            return redirect('students:student_edit', student_id)
-        else:
-            return render(request, 
-                          'add_edit.html', {'form': form, 'page_title':page_title})
-    else:
-        form = StudentForm(instance=student)   
-    return render(request, 'add_edit.html', {'form': form, 'page_title':page_title})
-'''
+    def get_context_data(self, **kwargs):
+        context = super(StudentDeleteView, self).get_context_data(**kwargs)
+        context['page_title'] = u"Удаление студента"
+        context['page_header'] = u"Студент {0} будет удалён".format(self.object.full_name())
+        return context
 
-def student_remove(request, student_id):
-    student = get_object_or_404(Student, pk=student_id)
-    full_name = student.full_name()
-    page_title = u"Удаление студента"
-    page_header = u"Студент {0} будет удалён".format(full_name)
-    if request.method == 'POST':
-        student.delete()
-        messages.success(request,u"Студент {0} был удалён"\
-                         .format(full_name))
-        return redirect('students:student_list')
-    return render(request, 'remove.html', {'student': student, 'page_title':page_title, 'page_header':page_header})
+    def delete(self, request, *args, **kwargs):
+        response = super(StudentDeleteView, self).delete(self, request, *args, **kwargs)
+        name = self.object.full_name()
+        messages.success(request, u"Студент {0} был удалён".format(name))
+        return response
 
