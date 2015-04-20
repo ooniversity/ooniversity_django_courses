08fbf98dd265e55f11a663ea7dfecf5b85d0ad9e
Tim Rustamov <gtimzua@gmail.com>
2015-04-20 10:01:27 -0700

    superduper

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,7 @@ from courses import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/add_lesson/$', views.add_lesson_to_course, name='add_lesson'),
+    url(r'^(?P<question_id>\d+)/edit_lesson/$', views.edit_lesson_in_course, name='edit_lesson'),
+    url(r'^(?P<question_id>\d+)/delete_lesson/$', views.delete_lesson_from_course, name='delete_lesson'),
 )
