7ba0aa5da8a88b667d248e39ddd6a5480e60e938
Oksana Slusarenko <oksana.slu@gmail.com>
2015-04-20 12:12:04 +0300

    Revert "S karakash"

--- coaches/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from coaches.views import coach_detail
-
-
-urlpatterns = patterns('',
-    url(r'^(?P<coach_id>\d+)/$', coach_detail, name='coach_detail'),
-)
--- quadratic/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from views import results
-
-
-urlpatterns = patterns('',
-    
-    url(r'^results/$', results, name='results'),
-)
