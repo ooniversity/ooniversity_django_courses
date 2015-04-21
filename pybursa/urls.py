19c1c60bd035aed473215dc0b34d941a62a3a0cc
Ivan Simeyko <iv.simeyko@gmail.com>
2015-04-21 07:29:34 +0300

    Task 9.1 - end

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from pybursa.views import *
-from courses.views import *
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name='home'),
     #url(r'^$', index_courses, name='home'),
-    url(r'^contact/$', contact, name='contact'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     #url(r'^student_list/$', student_list, name='student_list'),
     #url(r'^student_detail/$', student_detail, name='student_detail'),
     url(r'^quadratic/', include('quadratic.urls', namespace="quadratic")),
