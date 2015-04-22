c7889d215c4c15111f261c764d9f8d0d744646cb
Paha <paha@ubuntu.ubuntu-domain>
2015-04-23 02:21:08 +0300

    Change views courses and students

--- courses/views.py
+++ courses/views.py
@@ -1,67 +1,66 @@
 # -*- coding: UTF-8 -*-
 
 from django import forms
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from courses.models import Course, Lesson
+from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 
 
-def main(request):
-    courses_list = Course.objects.all().order_by('id')
-    return render(request, 'courses/main.html', {'courses_list': courses_list})
-
-def course_info(request, course_id):
-    current_course = Course.objects.get(id=course_id)
-    lessons_list = Lesson.objects.filter(course__id=course_id).order_by('number_order')
-    return render(request, 'courses/course_info.html', {'current_course': current_course, 'lessons_list': lessons_list})
-
-def contacts(request):
-    return render(request, 'courses/contacts.html')
+class CourseListView(ListView):
+    model = Course
+    template_name ='courses/main.html'
+    context_object_name = 'courses_list'
+    queryset = Course.objects.all().order_by('id')
 
 
-class CourseApplicationForm(forms.ModelForm):
-    class Meta:
-        model = Course
+class CourseDetailView(DetailView):
+    model = Course
+    template_name ='courses/course_info.html'
+    context_object_name = 'current_course'
 
 
-def course_add(request):
-    if request.method == 'POST':
-        form = CourseApplicationForm(request.POST)
-        if form.is_valid():
-            application = form.save()
-            messages.success(request, u'Курс %s успешно добавлен' %(application.name))
-            return redirect('main')
-    else:
-        form = CourseApplicationForm()
-    return render(request, 'courses/course_add.html', {'form': form})
+class CourseCreateView(CreateView):
+    model = Course
+    template_name ='courses/course_add.html'
+    success_url = reverse_lazy('main')
 
-def course_edit(request, course_id):
-    application = Course.objects.get(id=course_id)
-    if request.method == 'POST':
-        form = CourseApplicationForm(request.POST, instance=application)
-        if form.is_valid():
-            application = form.save()
-            messages.success(request, u'Данные о курсе %s изменены' %(application.name))
-            return redirect('courses:course_edit', application.id)
-    else:
-        form = CourseApplicationForm(instance=application)
-    return render(request, 'courses/course_edit.html', {'form': form, 'application': application})
-
-def course_remove(request, course_id):
-    application = Course.objects.get(id=course_id)
-    if request.method == 'POST':
-        application.delete()
-        messages.success(request, u'Курс %s удален' %(application.name))
-        return redirect('main')
-    return render(request, 'courses/course_remove.html', {'application': application})
+    def form_valid(self, form):
+        instance = form.save()
+        messages.success(self.request, u'Курс %s успешно добавлен' 
+                                            %(instance.name))
+        return super(CourseCreateView, self).form_valid(form)
 
 
+class CourseUpdateView(UpdateView):
+    model = Course
+    template_name ='courses/course_edit.html'
 
+    def form_valid(self, form):
+        messages.success(self.request, u'Данные о курсе %s изменены' 
+                                            %(self.object.name))
+        return super(CourseUpdateView, self).form_valid(form)
+
+
+class CourseDeleteView(DeleteView):
+    model = Course
+    template_name ='courses/course_remove.html'
+    context_object_name = 'application'
+    success_url = reverse_lazy('main')
+
+    def delete(self, request, *args, **kwargs):
+        response = super(CourseDeleteView, self).delete(request, *args, **kwargs)
+        messages.success(self.request, u'Курс %s успешно удален' 
+                                            %(self.object.name))
+        return response
 
 class LessonApplicationForm(forms.ModelForm):
     class Meta:
         model = Lesson
-        
 
 def add_lesson(request, course_id):
     if request.method == 'POST':
@@ -71,5 +70,6 @@ def add_lesson(request, course_id):
             messages.success(request, u'Занятие %s было создано' %(application.topic))
             return redirect('courses:course_info', application.course_id)
     else:
-        form = LessonApplicationForm()
+        form = LessonApplicationForm(initial={'course': course_id})
     return render(request, 'courses/add_lesson.html', {'form': form})
+
--- students/urls.py
+++ students/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 from students import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.students_all, name='students_all'),
-    url(r'^(?P<student_id>\d+)/$', views.student_info, name='student_info'),
-    url(r'^add/$', views.student_add, name='student_add'),
-    url(r'^edit/(?P<student_id>\d+)/$', views.student_edit, name='student_edit'),
-    url(r'^remove/(?P<student_id>\d+)/$', views.student_remove, name='student_remove'),
+	url(r'^$', views.StudentListView.as_view(), name='students_all'),
+    url(r'^(?P<pk>\d+)/$', views.StudentDetailView.as_view(), name='student_info'),
+    url(r'^add/$', views.StudentCreateView.as_view(), name='student_add'),
+    url(r'^edit/(?P<pk>\d+)/$', views.StudentUpdateView.as_view(), name='student_edit'),
+    url(r'^remove/(?P<pk>\d+)/$', views.StudentDeleteView.as_view(), name='student_remove'),
 )
