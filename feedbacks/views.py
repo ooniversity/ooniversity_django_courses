b7d3cb73e3d1fbb354f080d9a42fff3d2e288b87
Anna Kostenko <blackqueennn@gmail.com>
2015-04-25 21:19:23 +0300

    change Feedback, update message

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -13,6 +13,10 @@ class FeedbackCreateView(CreateView):
 
     def form_valid(self, form):
         response = super(FeedbackCreateView, self).form_valid(form)
+        send_message = 'Message from Name:{0},E-mail:{1} \n\n {2} \n Send at {3}'.format(
+            self.object.sender_name, self.object.sender_email, self.object.message,
+            self.object.date_create.strftime("%d.%m.%Y %H:%M:%S")
+        )
         messages.success(self.request, u'Ваше письмо успешно отправлено!')
-        mail_admins(self.object.theme, self.object.message, fail_silently=False)
+        mail_admins(self.object.theme, send_message, fail_silently=False)
         return response
