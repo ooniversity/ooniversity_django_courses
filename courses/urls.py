b400e58a2ff8a6a3343e09383bf3aa2fc3c1a780
andreyo <ao.mail@ua.fm>
2015-04-25 20:31:40 +0300

    task 9-2. done

--- courses/urls.py
+++ courses/urls.py
@@ -6,10 +6,10 @@ from courses import views_lesson
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
-    url(r'^add/', views.course_add, name='add'),
-    url(r'^edit/(?P<pk>\d+)/$', views.course_edit, name='edit'),
-    url(r'^remove/(?P<pk>\d+)/$', views.course_remove, name='remove'),
+    url(r'^(?P<pk>\d+)/$', views.ItemDetailView.as_view(), name='detail'),
+    url(r'^add/', views.ItemCreateView.as_view(), name='add'),
+    url(r'^edit/(?P<pk>\d+)/$', views.ItemUpdateView.as_view(), name='edit'),
+    url(r'^remove/(?P<pk>\d+)/$', views.ItemDeleteView.as_view(), name='remove'),
     # lessons
     url(r'^(?P<pk>\d+)/add_lesson/', views_lesson.item_add, name='add_lesson'),
 )
