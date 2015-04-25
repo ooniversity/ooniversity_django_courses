190ae1e7565ac14b93e098f10d43ec3cd996ded5
Kristina Bugaenko <agata1991kristi@gmail.com>
2015-04-25 23:20:41 +0300

    Add settings for send email

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -60,6 +60,16 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
+)
 
 ROOT_URLCONF = 'pybursa.urls'
 
@@ -98,4 +108,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-ADMINS =(())
+EMAIL_HOST = '127.0.0.1'
+EMAIL_PORT = 1025
+ADMINS =('peter@gmail.com', )
