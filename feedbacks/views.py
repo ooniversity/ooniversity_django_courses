394bbeec3769e183e31f3b7992b6436f8b1ef74b
Ivan Simeyko <iv.simeyko@gmail.com>
2015-04-25 12:08:47 +0300

    Task 10 - fixed bugs in view. Rev 1

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -10,6 +10,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic.edit import FormView
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import mail_admins
 
 
 
@@ -28,6 +29,36 @@ class ContactFormView(FormView):
     template_name = 'feedbacks/contact_form.html'
     success_url = 'feedbacks:contact_form'
 
+    def get_context_data(self, **kwargs):
+        context = super(ContactFormView, self).get_context_data(**kwargs)
+        context['page_title'] = u'Связаться с администратором сайта'
+        return context
+
+    '''
+    def form_valid(self, form):
+        name = self.request.POST.get('name', '')
+        subject = self.request.POST.get('subject', '')
+        message = self.request.POST.get('body', '')
+        from_email = self.request.POST.get('email', '')
+        send_mail(subject, message, from_email, ['admin@gmail.com'])
+        return super(ContactFormView, self).form_valid(form)
+    '''
+
+    def form_valid(self, form):
+        #form = super(ContactFormView, self).form_valid(form)
+        name = self.request.POST.get('name', '')
+        subject = self.request.POST.get('subject', '')
+        message = self.request.POST.get('body', '')
+        from_email = self.request.POST.get('email', '')
+        if name and subject and message and from_email:
+            mail_admins(subject, message, fail_silently=True)
+            messages.success(self.request, 'Mail successfully send')
+            return HttpResponseRedirect('#')
+        else:
+            return HttpResponse('Make sure all fields are entered and valid.')
+
+
+'''
     def save(self, *args, **kwargs):
         response = super(ContactFormView, self).save(*args, **kwargs)  # Call the "real" save() method.
         if self.request.method == "POST":
@@ -40,25 +71,4 @@ class ContactFormView(FormView):
                 send_mail(subject, message, from_email, ['admin@gmail.com'])
                 messages.success(self.request, "Message from %s was send" % name)
         return response
-
-
-    def get_context_data(self, **kwargs):
-        context = super(ContactFormView, self).get_context_data(**kwargs)
-        context['page_title'] = u'Создание письма'
-        return context
-    '''
-    def form_valid(self, form):
-        response = super(ContactFormView, self).form_valid(form)
-        name = self.request.POST.get('name', '')
-        subject = self.request.POST.get('subject', '')
-        message = self.request.POST.get('body', '')
-        from_email = self.request.POST.get('email', '')
-        if name and subject and message and from_email:
-            try:
-                send_mail(subject, message, from_email, ['admin@gmail.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return HttpResponseRedirect('#')
-        else:
-            return HttpResponse('Make sure all fields are entered and valid.')
-    '''
+        '''
