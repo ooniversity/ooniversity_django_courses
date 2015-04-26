3f266c2751d1cb538121ae92c5c21bf07f865ba0
Olga Kotulska <o.kotulska@gmail.com>
2015-04-26 17:22:27 +0300

    Feedback 2

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from . import views
-from django.views.generic.edit import CreateView
+from django.views.generic import View
 
 
 urlpatterns = [
-    url(r'^$', views.FeedbackCreateView.as_view(), name='feedback_form'), 
+    url(r'^$', views.FeedbackView.as_view(), name='feedback_form'), 
 ]
--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,10 +1,11 @@
 # -*- coding: UTF-8 -*-
-from django.shortcuts import render,  redirect
+from django.shortcuts import render,  redirect, HttpResponseRedirect
 from django.core.urlresolvers import reverse_lazy
 from django import forms
 from django.core.mail import mail_admins
 from django.contrib import messages
-from django.views.generic.edit import CreateView
+# from django.views.generic.create import CreateView
+from django.views.generic import View
 from models import Feedback
 
 
@@ -14,24 +15,46 @@ class FeedbackForm(forms.ModelForm):
 		fields = '__all__'
 
 
-class FeedbackCreateView(CreateView):
-	model = Feedback
-	template_name = "feedback_form.html"
-	success_url = reverse_lazy('feedbacks:feedback_form')
+# class FeedbackCreateView(CreateView):
+# 	model = Feedback
+# 	template_name = "feedback_form.html"
+# 	success_url = reverse_lazy('feedbacks:feedback_form')
 
-	def get_context_data(self, **kwargs):
-		context = super(FeedbackCreateView, self).get_context_data(**kwargs)
-		context['page_title'] = u'Заполните контактную форму'
-		context['button_value'] = u'Отправить'
-		return context
+# 	def get_context_data(self, **kwargs):
+# 		context = super(FeedbackCreateView, self).get_context_data(**kwargs)
+# 		context['page_title'] = u'Заполните контактную форму'
+# 		context['button_value'] = u'Отправить'
+# 		return context
 
-	def form_valid(self, form):
-		response = super(FeedbackCreateView, self).form_valid(form)
-		mail_admins(subject=self.object.subject, 
-			        message=self.object.text_message, 
+# 	def form_valid(self, form):
+# 		response = super(FeedbackCreateView, self).form_valid(form)
+# 		mail_admins(subject=self.object.subject, 
+# 			        message=self.object.text_message, 
+# 			        fail_silently=False, 
+# 			        connection=None, 
+# 			        html_message=None)
+# 		name = self.object.subject
+# 		messages.info(self.request, u"Сообщение '%s' отправлено" % name)
+# 		return response
+
+class FeedbackView(View):
+	form_class = FeedbackForm
+	template_name = 'feedback_form.html'
+
+	def get(self, request, *args, **kwargs):
+		form = self.form_class()
+		return render(request, self.template_name, {'form': form, 'page_title': u'Заполните контактную форму', 'button_value': u'Отправить'})
+
+	def post(self, request, *args, **kwargs):
+		form = self.form_class(request.POST)
+		if form.is_valid():
+			new_feedback = form.save()
+			mail_admins(subject=new_feedback.subject, 
+			        message=new_feedback.text_message, 
 			        fail_silently=False, 
 			        connection=None, 
 			        html_message=None)
-		name = self.object.subject
-		messages.info(self.request, u"Сообщение '%s' отправлено" % name)
-		return response
+			messages.success(request, u"Сообщение %s отправлено" % new_feedback.subject)
+			return redirect('feedbacks:feedback_form')
+		return render(request, self.template_name, 
+        	{'form': form, 'page_title': u'Заполните контактную форму', 'button_value': u'Отправить'})	
