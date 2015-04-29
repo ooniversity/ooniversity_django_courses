b36c29a949c52a59af0f8f041639c43780fa89c9
Yaroslav Juvanov <pyromanser@mail.ru>
2015-04-29 12:44:48 +0300

    Made tests for courses and students.

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+from courses.models import Course
+
+
+class CoursesTests(TestCase):
+    def test_pages(self):
+        client = Client()
+
+        response_courses = client.get('/courses/')
+
+        self.assertEqual(response_courses.status_code, 200)
+
+    def test_course_create(self):
+        client = Client()
+
+        course = Course.objects.create(
+            name="Python",
+            short_description='Some text.',
+            description='Some long text.',
+            )
+
+        self.assertEqual(Course.objects.all().count(), 1)
+
+        course_another = Course.objects.create(
+            name="Django",
+            short_description='Some text.',
+            description='Some long text.',
+            )
+
+        self.assertEqual(Course.objects.all().count(), 2)
+
+        response = client.get('/courses/')
+
+        for item in Course.objects.all():
+            self.assertContains(response, item)
+
+    def test_course_detail(self):
+        client = Client()
+
+        course = Course.objects.create(
+            name="Python",
+            short_description='Some text.',
+            description='Some long text.',
+            )
+
+        response = client.get('/courses/%s/' % Course.objects.get().pk)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, course)
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.test import Client
+
+from students.models import Student
+
+
+class StudentsTests(TestCase):
+    def test_pages(self):
+        client = Client()
+        
+        response_students = client.get('/students/')
+
+        self.assertEqual(response_students.status_code, 200)
+
+    def test_student_create(self):
+        client = Client()
+
+        student = Student.objects.create(
+            name='Name',
+            surname='Surname',
+            date_of_birth=datetime.date.today(),
+            email='example@example.com',
+            phone='+380501234567',
+            skype='skype',
+            )
+
+        self.assertEqual(Student.objects.all().count(), 1)
+
+        student_another = Student.objects.create(
+            name='Vasya',
+            surname='Pupkin',
+            date_of_birth=datetime.date.today(),
+            email='another@example.com',
+            phone='+380507654321',
+            skype='another',
+            )
+
+        self.assertEqual(Student.objects.all().count(), 2)
+
+        response = client.get('/students/')
+
+        for item in Student.objects.all():
+            self.assertContains(response, item)
+
+    def test_student_detail(self):
+        client = Client()
+
+        student = Student.objects.create(
+            name='Vasya',
+            surname='Pupkin',
+            date_of_birth=datetime.date.today(),
+            email='example@example.com',
+            phone='+380501234567',
+            skype='skype',
+            )
+
+        response = client.get('/students/%s/' % Student.objects.get().pk)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, student)
