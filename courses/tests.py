8625958d45e4e3fef57f8def577512e61cc2cf31
Ivan Simeyko <iv.simeyko@gmail.com>
2015-05-01 16:05:20 +0300

    Task 11/2, create test - rev.1

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
 
-# Create your tests here.
+
+from django.test import TestCase
+from courses.models import Course
+from django.test import Client
+
+
+class CourseTests(TestCase):
+
+    # тест Алексея из видео
+    def test_course_create_object(self):
+        # создание объектов курсов
+        course1 = Course.objects.create(
+            name='Python/Django',
+            short_description=u'Профессиональные Python курсы'
+        )
+        course2 = Course.objects.create(
+            name='',
+            short_description=u''
+        )
+        # проверка что курсы создались
+        self.assertEqual(Course.objects.all().count(), 2)
+
+    # тест списка курсов
+    def test_courses_list(self):
+        # запуск проекта
+        client = Client()
+        # переходим на страницу списка курсов
+        response = client.get('/')
+        # проверка что страничка работает
+        self.assertEqual(response.status_code, 200)
+        # проверка что б.д. пуста
+        self.assertEqual(Course.objects.all().count(), 0)
+        # создадим курс
+        course1 = Course.objects.create(
+            name='Python/Django',
+            short_description=u'Профессиональные Python курсы'
+        )
+        # проверим что курс создался
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, u'Профессиональные Python курсы')   # почему-то 'Python/Django' не сработало
+        self.assertEqual(Course.objects.all().count(), 1)
+
+    # тест создания курса
+    def test_course_create(self):
+        # запуск сервера
+        client = Client()
+        # проверяем что курса нет
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 404)
+        # создаем курс
+        course1 = Course.objects.create(
+            name='Python/Django',
+            short_description=u'Профессиональные Python курсы'
+        )
+        # проверка что курс создался
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Python/Django")
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
 
-# Create your tests here.
+from django.test import TestCase, Client
+from students.models import Student
+from datetime import date
+
+
+class StudentsTests(TestCase):
+
+    # тест списка студентов
+    def test_students_list(self):
+        # запуск проекта
+        client = Client()
+        # переходим на страницу списка студентов
+        response = client.get('/students/')
+        # проверка что страничка работает
+        self.assertEqual(response.status_code, 200)
+        # проверка что список пока пуст (в б.д. ноль студентов)
+        self.assertEqual(Student.objects.all().count(), 0)
+        #self.assertNotContains(response, u'Булгаков')
+        # создадим студента
+        student1 = Student.objects.create(name=u'Миша', surname=u'Булгаков', date_of_birth=date(1891, 05, 03),
+                                          email='master@margarita.com', phone='321', address=u'Киев-Москва',
+                                          skype='Misha')
+        # проверим что студент появился в списке
+        response = client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, u'Миша')
+        self.assertEqual(Student.objects.all().count(), 1)
+
+    # тест информации про студента
+    def test_students_detail(self):
+        client = Client()
+        response = client.get('/students/1/')
+        # проверка что страницы не существует
+        self.assertEqual(response.status_code, 404)
+        # создание студента
+        student1 = Student.objects.create(name=u'Миша', surname=u'Булгаков', date_of_birth=date(1891, 05, 03),
+                                          email='master@margarita.com', phone='321', address=u'Киев-Москва',
+                                          skype='Misha')
+        response = client.get('/students/1/')
+        # проверка странички
+        self.assertEqual(response.status_code, 200)
+        # проверка наличия информации о сдуденте на стриничке
+        self.assertContains(response, u'Миша Булгаков')
+        # провека количества студентов в б.д.
+        self.assertEqual(Student.objects.all().count(), 1)
