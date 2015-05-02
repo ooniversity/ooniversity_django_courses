273b571725b28630ec4dbab2ac3977e6545126cc
Volodya Esk <vkhatianovskyi@gmail.com>
2015-05-02 19:19:37 +0300

    backup - something going wrong, need some changes in courses.models

--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.test import Client
+from students.models import Student
+from datetime import date
 
-# Create your tests here.
+class StudentTest(TestCase):
+ 
+    def test_list_students(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_student_create(self):
+        client = Client()
+        student1 = Student.objects.create(
+            name = "Nickolas",
+            surname = 'Popandopolas	',
+            date_of_birth = date(1989, 12, 19),
+            email = 'np@gmail.com',
+            phone = '123456789',
+            address = 'address',
+            skype = 'np',
+            )
+        response = client.get('/students/')
+        self.assertEqual(Student.objects.all().count(), 1)
+        self.assertContains(response, "Nickolas")
+
+        # checking the detail info about student
+        response = client.get('/students/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Popandopolas')
+        
