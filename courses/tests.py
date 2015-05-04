b9330f95a29c0ed38ba1b6cfdd97540076178351
Oleg <oleg_ishchenko@i.ua>
2015-05-04 23:05:21 +0300

    changed gitignore

--- courses/tests.py
+++ courses/tests.py
@@ -1,1 +1,34 @@
 from django.test import TestCase
+
+from courses.models import Course
+
+
+class CoursesTests(TestCase):
+
+	def test_course_list(self):
+		from django.test import Client
+		client = Client()
+
+		response = client.get('/')
+		
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(Course.objects.all().count(), 0)
+
+	def test_course_page(self):
+		from django.test import Client
+		client = Client()
+
+		response = client.get('/courses/1/')
+
+		self.assertEqual(response.status_code, 404)
+
+		course1 = Course.objects.create(
+			title = 'Testing title', 
+			short_description = 'Testing short description',
+			description = 'Testing description'
+			)
+
+		response = client.get('/courses/1/')
+		self.assertEqual(response.status_code, 200)
+
+		self.assertEqual(Course.objects.all().count(), 1)
--- students/tests.py
+++ students/tests.py
@@ -1,24 +1,37 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 
-from courses.models import Course
+from students.models import Student
 
-class CourseTests(TestCase):
+class StudentsTests(TestCase):
 
-	def test_course_create(self):
-		course1 = Course.objects.create(title = 'PyBursa02',
-										short_description = "Web development with django")
-		self.assertEqual(Course.objects.all().count(), 1)
-
-	def test_pages(self):
+	def test_student_list(self):
 		from django.test import Client
 		client = Client()
 
-		response = client.get('/courses/1/')
+		response = client.get('/students/')
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(Student.objects.all().count(), 0)
+
+	def test_student_page(self):
+		from django.test import Client
+		client = Client()
+
+		response = client.get('/students/1/')
+
 		self.assertEqual(response.status_code, 404)
 
-		course1 = Course.objects.create(title='PyBursa02',
-										short_description="Web development with django")
+		student1 = Student.objects.create(
+			name = 'Grigoriy', 
+			surname = 'Grigoriev',
+			birth_date = '1990-02-10',
+			email = 'namesurname@ukr,net',
+			phone = '0668787678',
+			adress = 'Kharkiv',
+			skype = 'namesurname'
+			)
 
-		response = client.get('/courses/1/')
+		response = client.get('/students/1/')
 		self.assertEqual(response.status_code, 200)
-		self.assertContains(response, 'Pybursa02')
+
+		self.assertEqual(Student.objects.all().count(), 1)
