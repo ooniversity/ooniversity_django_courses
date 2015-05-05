7b61c2a9f3bc118b17107c21ec548d56ab093268
Andrey Sviridov <sviridoff@inbox.ru>
2015-05-05 20:43:50 +0300

    11.2

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from courses.models import Course
 
-# Create your tests here.
+class CoursesTests(TestCase):
+
+    def test_courses(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+        test1 = Course.objects.create(name='Testcourse1', short_description='Short', description='Full')
+        self.assertEqual(Course.objects.all().count(), 1)
+        response = c.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Testcourse1')
+        test2 = Course.objects.create(name='Testcourse2', short_description='Short', description='Full')
+        self.assertEqual(Course.objects.all().count(), 2)
+        response = c.get('/courses/2/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Testcourse2')
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from students.models import Student
+from datetime import date
 
-# Create your tests here.
+class StudentsTests(TestCase):
+    
+    def test_students(self):
+        c = Client()
+        response = c.get('/students/')
+        self.assertEqual(response.status_code, 200)
+        test1 = Student.objects.create(name='Ivan', surname='Ivanov',
+                                        date_of_birth=date(1984, 02, 16), email='ivan@testing.com',
+                                        phone='+380000', address='example address', skype='ivan')
+        response = c.get('/students/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Student.objects.all().count(), 1)
+        self.assertContains(response, 'Ivanov')
+        test2 = Student.objects.create(name='Petr', surname='Petrov',
+                                        date_of_birth=date(1984, 02, 16), email='petrov@testing.com',
+                                        phone='+380000', address='example address', skype='petr')
+        response = c.get('/students/2/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Student.objects.all().count(), 2)
+        self.assertContains(response, 'Petrov')
