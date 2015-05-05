73f41a24d817e4412df555c392ae9144f9fb20c6
Oleg <oleg_ishchenko@i.ua>
2015-05-05 21:29:51 +0300

    Task 11.2 finished

--- courses/tests.py
+++ courses/tests.py
@@ -1,25 +1,36 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 
-from courses.models import Course
+from courses.models import Course, Lesson
 
 
 class CoursesTests(TestCase):
 
 	def test_course_list(self):
 		from django.test import Client
-		client = Client()
 
-		response = client.get('/')
-		
+		client = Client()
+		response = client.get('/')		
 		self.assertEqual(response.status_code, 200)
 		self.assertEqual(Course.objects.all().count(), 0)
 
+
+		course1 = Course.objects.create(
+			title = 'Testing title', 
+			short_description = 'Testing short description',
+			description = 'Testing description'
+			)
+
+		self.assertEqual(Course.objects.all().count(), 1)
+		response = client.get('/')
+		self.assertContains(response, 'Testing title')
+		
+
 	def test_course_page(self):
 		from django.test import Client
+
 		client = Client()
-
 		response = client.get('/courses/1/')
-
 		self.assertEqual(response.status_code, 404)
 
 		course1 = Course.objects.create(
@@ -28,7 +39,6 @@ class CoursesTests(TestCase):
 			description = 'Testing description'
 			)
 
-		response = client.get('/courses/1/')
+		response = client.get('/courses/1/')		
 		self.assertEqual(response.status_code, 200)
-
 		self.assertEqual(Course.objects.all().count(), 1)
