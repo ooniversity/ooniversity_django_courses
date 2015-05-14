8fbb8418fd094bb69fe1226d4800caaee74a7ac6
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2015-05-14 08:43:34 +0300

    Add to the main page

--- pybursa/views.py
+++ pybursa/views.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import random;
+
 from django.shortcuts import render
 from courses.models import Course
 #from pybursa import urls
@@ -6,8 +8,24 @@ from courses.models import Course
 
 def show_index(request):
     courses = Course.objects.all()
+    try:
+        with open('static/text.txt', 'r') as text_file:
+            text = text_file.read().decode('utf-8')
+    except IOError:
+        print "Error! File text.txt not found."
+    excerption = random.choice(text.split('\n'))
+    number = text.split('\n').index(excerption)
+
+    try:
+        with open('static/autors_text.txt', 'r') as text_file:
+            text = text_file.read()
+    except IOError:
+        print "Error! File autors_text.txt not found."
+    autor = text.split('\n')[number]
+
     return render(request, 'index.HTML',
-                 {'courses': courses})
+                 {'courses': courses, 'excerption': excerption,
+                 'autor': autor,})
 
 
 def show_contacts(request):
